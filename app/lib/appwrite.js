e34191aeddf5bc79bf85fda225be1828e0fef82c
701-extreme <evenlyrl@gmail.com>
2025-12-24 09:24:17 +1300

    remove app

--- app/lib/appwrite.js
+++
@@ -1,9 +0,0 @@
-import {Client, Account, Avatars } from 'react-native-appwrite';
-
-// tells which project to connect to and the endpoint URL
-const client = new Client()
-    .setProject("691d5bf6000e06f2505c")
-    .setEndpoint("https://syd.cloud.appwrite.io/v1");
-
-export const account = new Account(client); // create account instance to backend, for auth purposes
-export const avatars = new Avatars(client);
